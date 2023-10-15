b26814229ac99bdc4e115597d2b998d9ec3595a7
G-nizam-A <nizamcs10@gmail.com>
2023-10-15 22:01:37 +0530

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 // server.js
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+// const path = require('path')  
 
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
 
 app.get('/index', function (req, res) {
     res.send('index')
   });
-  app.get('/', function (req, res) {
-    res.send('hello')
-  });
+// // Select all data
+app.get('/', (req, res) => {
+    pool.query('SELECT * FROM post', (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        res.json(results);
+    });
+});
 
 const port = process.env.PORT | 3000;
 app.listen(port, () => {
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// // server.js
+
 // const express = require('express');
 // const mysql = require('mysql2');
 // const app = express();
