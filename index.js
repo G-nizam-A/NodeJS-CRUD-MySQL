1d1eb2311ad27d1ab89c89e977d260db67490049
G-nizam-A <nizamcs10@gmail.com>
2023-10-15 21:56:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', function (req, res) {
+app.get('/index', function (req, res) {
     res.send('index')
+  });
+  app.get('/', function (req, res) {
+    res.send('hello')
   });
 
 const port = process.env.PORT | 3000;
