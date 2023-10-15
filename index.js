cebd55598598a3540f19e5dbea7dae0f4a650c61
G-nizam-A <nizamcs10@gmail.com>
2023-10-15 21:52:58 +0530

    first commit

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.delete('/posts/del/:id', (req, res) => {
     });
 });
 
-const port = process.env.port | 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
